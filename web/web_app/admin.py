8107813e12ee1c9fee14579249fc30dbe84c4103
AngelinaL04 <angelinalaw02@gmail.com>
2024-10-31 17:23:28 +0700

    first commit

---
+++ web/web_app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from web_app.models import StatusModel, GeeksModel, Album, Song, Author, Book, Vehicle, Car
+
+admin.site.register(StatusModel)
+admin.site.register(GeeksModel)
+admin.site.register(Album)
+admin.site.register(Song)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Vehicle)
+admin.site.register(Car)
+
